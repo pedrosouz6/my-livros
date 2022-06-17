ae22df31af5be4aa1ae24da260c720d955e7494f
Pedro Souza <peedrosouz6@gmail.com>
2022-06-17 17:10:13 -0300

    page wellcome

--- components/Public/Wellcome/index.tsx
+++ components/Public/Wellcome/index.tsx
@@ -1,9 +1,32 @@
-import { ContainerWellcome } from "./style";
+import Image from "next/image";
+import ImageWellcome from '../../../assets/Wellcome.svg';
+
+import { 
+    Container, 
+    ContainerWellcome,
+    TitleWellcome, 
+    LeftWellcome, 
+    RightWellcome 
+} from "./style";
+
+import { ContainerCenter } from "../../../styles/style";
 
 export function PublicWellcome() {
     return (
-        <ContainerWellcome>
-            <h1>organize seus livros</h1>
-        </ContainerWellcome>
+        <Container>
+            <ContainerCenter>
+                <ContainerWellcome>
+                    <LeftWellcome>
+                        <TitleWellcome>Organize seus livros</TitleWellcome>
+                        <p>O MyLivros irá te ajudar a organizar seus livros online de forma fácil e gratuita!</p>
+                        <button>Começar</button>
+                    </LeftWellcome>
+
+                    <RightWellcome>
+                        <Image src={ImageWellcome} alt='Imagem de boas vindas' width='400px' height='400px' />
+                    </RightWellcome>
+                </ContainerWellcome>
+            </ContainerCenter>
+        </Container>
     )
 }
--- components/Public/Wellcome/style.ts
+++ components/Public/Wellcome/style.ts
@@ -1,5 +1,57 @@
 import styled from "styled-components";
 
+import { Theme } from "../../../theme";
+
+const {
+    main
+} = Theme.light;
+
+export const TitleWellcome = styled.h1 `
+    text-align: center;
+    color: ${main};
+    font-size: 3.3rem;
+`
+
+export const Container = styled.div `
+    padding: 30px 0;
+`
+
 export const ContainerWellcome = styled.div `
-    
+    display: flex;
+    gap: 2rem;
+    flex-wrap: wrap;
+    align-items: center;
+`
+
+export const LeftWellcome = styled.div `
+    flex: 1 1 300px;
+
+    height: 100%;
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    gap: 2rem;
+
+    button {
+        padding: 10px 40px;
+        border-radius: 25px;
+        border: none;
+        outline: none;
+        background-color: ${main};
+        color: white;
+        cursor: pointer;
+    }
+
+    p {
+        text-align: center;
+        font-size: 1.4rem;
+    }
+
+`
+
+export const RightWellcome = styled.div `
+    flex: 1 1 300px;
+
+    display: flex;
+    justify-content: center;
 `
