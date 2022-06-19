74749b90faa6af45ead1385951944c7c758c1545
Pedro Souza <peedrosouz6@gmail.com>
2022-06-19 14:19:07 -0300

    deploy on vercel

---
+++ theme/index.ts
@@ -0,0 +1,9 @@
+export const Theme = {
+    light: {
+        white: '#f4f4f4',
+        black: '#333',
+        main: 'teal',
+        beige: '#D9D9D9',
+        transition: '.3s ease-in-out'
+    }
+}
